782bfced2989876c07c2fcba7e546d02ec3a5095
Pat <patrick.h.needham@gmail.com>
2018-07-30 22:27:40 -0400

    react routing code splitting -- looks cool, but the react router tutorial hasn't come up yet

---
+++ src/components/AsyncComponent.js
@@ -0,0 +1,31 @@
+// TODO: get back to this when looking at the react router
+// https://serverless-stack.com/chapters/code-splitting-in-create-react-app.html
+import React, { Component } from 'react';
+
+export default function asyncComponent(importComponent) {
+  class AsyncComponent extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        component: null
+      };
+    }
+
+    async componentDidMount() {
+      const { default: component } = await importComponent();
+
+      this.setState({
+        component: component
+      });
+    }
+
+    render() {
+      const C = this.state.component;
+
+      return C ? <C {...this.props} /> : null;
+    }
+  }
+
+  return AsyncComponent;
+}
